a6b54bd6882ebcf13fc65cb9f6677f2629773b5c
Guilherme Leobas <guilhermeleobas@gmail.com>
2025-02-12 17:24:15 -0300

    Allow setting attribute to NestedUserFunctionVariable
    ghstack-source-id: 361f3d2d1bc0396e148620416d293f8505a7b3f2
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/146505

--- torch/_dynamo/variables/builtin.py
+++ torch/_dynamo/variables/builtin.py
@@ -1841,6 +1841,7 @@ class BuiltinVariable(VariableTracker):
                 variables.PlacementVariable,
                 variables.NamedTupleVariable,
                 variables.UserDefinedObjectVariable,
+                variables.NestedUserFunctionVariable,
                 variables.ExceptionVariable,
             ),
         ):
